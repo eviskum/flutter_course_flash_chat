81c6707f6d04a7a6fcf3a2beb0bd5d5d99bd39cf
Esben Viskum <viskum@viskum.dk>
2021-11-13 17:15:16 +0100

    firebase dependencies updated

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -10,6 +10,9 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,14 +33,14 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             const SizedBox(height: 48.0),
             TextField(
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
               decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
             ),
             const SizedBox(height: 8.0),
             TextField(
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
               decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
             ),
